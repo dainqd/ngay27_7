0cf15fc42864ee6625b51bdf4ba7d44f335de594
dainq <97323194+dainqd@users.noreply.github.com>
2022-07-27 23:32:43 +0700

    Add files via upload

---
+++ vd1.cs
@@ -0,0 +1,29 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace LinqBasics
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Existing .NET Deatures");
+//            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6 };
+//            foreach (var number in numbers)
+//            {
+//                if (number < 3)
+//                    Console.WriteLine(number);
+//            }
+//            Console.WriteLine("Using LINQ");
+
+//            List<int> numbersLinq = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+//            IEnumerable<int> query = from n in numbersLinq
+//                                     where n < 3
+//                                     select n;
+//            foreach (int number in query)
+//                Console.WriteLine(number);
+//        }
+//    }
+//}
